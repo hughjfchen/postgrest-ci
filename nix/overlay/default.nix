dc446697deb81b02a2471bb221d046b74fc827a1
Hugh JF Chen <hugh.jf.chen@gmail.com>
2020-11-27 15:29:38 +0000

    override ssh ssl to build static lib

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -1,6 +1,8 @@
 self: prev: 
 rec
 { openssl = prev.openssl.override { static = true; };
+  libssh2 = (prev.libssh2.overrideAttrs (oldAttrs: { dontDisableStatic = true; 
+    configureFlags = [ "--disable-examples-build" ]; }));
   postgresql = (prev.postgresql.overrideAttrs (oldAttrs: { dontDisableStatic = true; })).override { enableSystemd = false; };
   libpq = postgresql;
 }
